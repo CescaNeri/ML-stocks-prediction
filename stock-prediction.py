3df9cc9ccfbc0b93bae8b0e60d772e37c2374439
Francesca Neri <104086181+CescaNeri@users.noreply.github.com>
2022-12-29 14:52:29 +0100

    add tech analysis

--- stock-prediction.py
+++ stock-prediction.py
@@ -22,7 +22,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Set functions
 
-def GetData(stock):
+def get_data(stock):
     return yf.Ticker(stock).history(period='max')
 
 def clean(df, del_list):
@@ -35,7 +35,7 @@ def target(df, t_name, target_name):
   df[target_name] = (df[t_name] > df['Close']).astype(int)
   return df
 
-def accuracy(model, test):
+def accuracy(model, test, predictors):
   preds = model.predict(test[predictors])
   preds = pd.Series(preds, index = test.index)
   solution = precision_score(test['Target'], preds)
@@ -43,13 +43,24 @@ def accuracy(model, test):
   combined = pd.concat({'Target': test['Target'],'Predictions': preds}, axis=1)
   combined.plot()
 
-def RandomForest(df, predictors, split):
+def random_forest(df, predictors, split):
   df = df.dropna()
   model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
   train = df.iloc[:split]
   test = df.iloc[split:]
   model.fit(train[predictors], train['Target'])
-  return accuracy(model, test)
+  return accuracy(model, test, predictors)
+
+def smooth_data(data, alpha):
+  return data.ewm(alpha=alpha).mean()
+
+def tech_analysis(df, indicators):
+  df.rename(columns = {'Close': 'close', 'High': 'high', 'Low': 'low', 
+                       'Adj Close': 'adj close', 'Volume': 'volume', 'Open': 'open'}, 
+            inplace = True)
+  for indicator in indicators:
+    df[indicator] = eval('TA.' + indicator + '(data)')
+  return df
 
 # Execute the Model
 
@@ -60,8 +71,8 @@ model_choice = input("""Select a Machine Learning model among these three:
     \n""")
 
 if model_choice == '1':
-  nvidia = GetData('NVDA')
-  bitcoin = GetData('BTC')
+  nvidia = get_data('NVDA')
+  bitcoin = get_data('BTC')
 
   to_del = ['Dividends', 'Stock Splits']
   clean(nvidia, to_del)
@@ -77,5 +88,25 @@ if model_choice == '1':
 
   predictors = ['Open', 'High', 'Low', 'Close', 'Volume', 't1', 'Open_B',
        'High_B', 'Low_B', 'Close_B', 'Volume_B', 't1_b', 'Target_b']
-  RandomForest(nvidia, predictors, -50)
+  random_forest(nvidia, predictors, -50)
+
+elif model_choice == '2':
+  nvidia = get_data('NVDA')
+
+  to_del = ['Dividends', 'Stock Splits']
+  clean(nvidia, to_del)
+  target(nvidia, 't1', 'Target')
+
+  alpha = 0.65
+  data = smooth_data(nvidia, alpha)
+  indicators = ['OBV', 'ADL', 'ADX', 'RSI', 'STOCH', 'SMA']
+  tech_analysis(data, indicators)
+
+  to_del = ['open', 'high', 'low', 'close', 'volume', 't1', 'Target']
+  clean(data, to_del)
+  nvidia = nvidia.merge(data, how='inner', on='Date')
+
+  predictors = ['Open', 'High', 'Low', 'Close', 'Volume', 't1', 'OBV', 'ADL',
+       'ADX', 'RSI', 'STOCH', 'SMA']
+  random_forest(nvidia, predictors, -50)
 
